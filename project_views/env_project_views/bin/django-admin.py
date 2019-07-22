0c65a9c441c9a99946784fbecda0be78c2d666f5
fikriamri <famri@alterra.id>
2019-07-22 10:48:47 +0700

    django projects

---
+++ project_views/env_project_views/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/alta7/Alta/DJANGO_MVC/project_views/env_project_views/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
